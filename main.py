c1c3970fbd01ff1b759ee81fed1e1b3fc5f60709
Ondiek Elijah <elie@localhost.localdomain>
2021-11-19 15:35:01 +0300

    modified: main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Introduction to": "FastAPI"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"Here are your": "FastAPI posts"}
+
+
+@app.post("/createpost")
+def create_posts(payload: dict=Body(...)):
+    print(payload)
+    return {"message": f"title {payload['title']} content: {payload['content']}"}
