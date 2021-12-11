94d0db7f5b15ee11e581947c2b0f7c1d53959405
Dev-Elie <eochieng9448@gmail.com>
2021-12-11 16:33:09 +0300

    Updated project to sqlalchemy ORM frm raw SQL

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:ondie@localhost/fastAPI"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
