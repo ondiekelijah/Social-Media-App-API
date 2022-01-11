fcc18511a5f9f990bdc3c769dbf36712349f49b7
Dev-Elie <eochieng9448@gmail.com>
2022-01-11 16:59:52 +0300

    Added User Tests

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,9 @@
 import pytest
+from jose import jwt
 from app import schemas
-from .database import client, session
+from app.config import settings
 
 
-def test_root(client):
-    res = client.get("/")
-    assert res.status_code == 200
-
-@pytest.fixture()
-def test_user(client):
-    user_data = {"email": "test@gmail.com", "password": "test123"}
-    res = client.post("/users/", json=user_data)
-    assert res.status_code == 201
-    return
 
 def test_create_user(client):
     res = client.post(
@@ -25,7 +16,27 @@ def test_create_user(client):
 
 def test_login_user(client, test_user):
     res = client.post(
-        "/login", data={"username": "test@gmail.com", "password": "test123"}
+        "/login",
+        data={"username": test_user["email"], "password": test_user["password"]},
     )
+    login_res = schemas.Token(**res.json())
+
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=settings.algorithm
+    )
+    id = payload.get("user_id")
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
 
     assert res.status_code == 200
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'test123', 403),
+    ('test@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'test123', 422),
+    ('test@gmail.com', None, 422)
+])
+def test_failed_login(test_user, client, email, password, status_code):
+    res = client.post("/login",data={"username": email, "password": password})
+    assert res.status_code == status_code
